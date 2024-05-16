e0d44683a68c6972e6af9aad5c657710e21222e5
¨Gabriel <erlonminievix@gmail.com>
2024-05-15 20:35:00 -0400

    add smartlamp.ino code

--- smartlamp.ino
+++ smartlamp.ino
@@ -1,40 +1,54 @@
-// Defina os pinos de LED e LDR
-// Defina uma variável com valor máximo do LDR (4000)
-// Defina uma variável para guardar o valor atual do LED (10)
-int ledPin;
-int ledValue;
+#include <Arduino.h>
 
-int ldrPin;
-int ldrMax;
+int ledPin = 13;
+int ledValue = 100;
+
+int ldrPin = 36;
+int ldrMax = 4000;
 
 void setup() {
     Serial.begin(9600);
-    
     pinMode(ledPin, OUTPUT);
     pinMode(ldrPin, INPUT);
     
-    Serial.printf("SmartLamp Initialized.\n");
-
-    
+    Serial.println("SmartLamp Initialized.");
 }
 
-// Função loop será executada infinitamente pelo ESP32
 void loop() {
-    //Obtenha os comandos enviados pela serial 
-    //e processe-os com a função processCommand
+    if (Serial.available() > 0) {
+        String command = Serial.readStringUntil('\n');
+        command.trim();
+        processCommand(command);
+    }
 }
-
 
 void processCommand(String command) {
-    // compare o comando com os comandos possíveis e execute a ação correspondente      
+    if (command.startsWith("SET_LED ")) {
+        int value = command.substring(8).toInt();
+        Serial.println("ok");
+        if (value >= 0 && value <= 100) {
+            ledUpdate(value);
+            Serial.println("RES SET_LED 1");
+        } else {
+            Serial.println("RES SET_LED -1");
+        }
+    } else if (command == "GET_LED") {
+        Serial.printf("RES GET_LED %d\n", ledValue);
+    } else if (command == "GET_LDR") {
+        int ldrValue = ldrGetValue();
+        Serial.printf("RES GET_LDR %d\n", ldrValue);
+    } else {
+        Serial.println("ERR Unknown command.");
+    }
 }
 
-// Função para atualizar o valor do LED
-void ledUpdate() {
-    // Normalize o valor do LED antes de enviar para a porta correspondente
+void ledUpdate(int newValue) {
+    ledValue = newValue;
+    int pwmValue = map(ledValue, 0, 100, 0, 255);
+    analogWrite(ledPin, pwmValue);
 }
 
-// Função para ler o valor do LDR
 int ldrGetValue() {
-    // Leia o sensor LDR e retorne o valor normalizado
+    int digitalValue = digitalRead(ldrPin);
+    return digitalValue == HIGH ? 100 : 0;
 }
