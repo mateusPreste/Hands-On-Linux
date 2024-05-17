e519980a02a9cd6436ceb54767c5d9a2286cb404
Bruno Negreiros <bruno.negreiros@eldorado.org.br>
2024-05-17 11:17:33 -0400

    feat: create .gitignore

--- smartlamp-kernel-module/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/devtitans-1/Desktop/git/Hands-On-Linux/smartlamp-kernel-module/modules.order := {   echo /home/devtitans-1/Desktop/git/Hands-On-Linux/smartlamp-kernel-module/probe.ko; :; } | awk '!x[$$0]++' - > /home/devtitans-1/Desktop/git/Hands-On-Linux/smartlamp-kernel-module/modules.order
--- smartlamp-kernel-module/probe.mod
+++
@@ -1,2 +0,0 @@
-/home/devtitans-1/Desktop/git/Hands-On-Linux/smartlamp-kernel-module/probe.o
-
