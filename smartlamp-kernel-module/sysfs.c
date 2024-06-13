57ee9e098495443a306f7ba3dadd6f9b8093ae58
Enzo H. Albuquerque <33371637+henriqu3nzo@users.noreply.github.com>
2024-06-12 20:07:05 -0400

    Henrique (#11)
    * mudanca na funcao attr_store()
    
    * attr_store() finalizada
    
    ---------
    
    Co-authored-by: ¨henriqu3nzo¨ <¨ehsa2210@gmail.com¨>

--- smartlamp-kernel-module/sysfs.c
+++ smartlamp-kernel-module/sysfs.c
@@ -157,12 +157,12 @@ static ssize_t attr_store(struct kobject *sys_obj, struct kobj_attribute *attr, 
         return -EACCES;
     }
 
-    printk(KERN_INFO "SmartLamp: Setando %s para %ld ...\n", attr_name, value);
-
-    if (ret < 0) {
-        printk(KERN_ALERT "SmartLamp: erro ao setar o valor do %s.\n", attr_name);
-        return -EACCES;
+    // Verifica o nome do atributo e executa ações específicas
+    if (strcmp(attr_name, "ldr") == 0) {
+        printk(KERN_ALERT "SmartLamp: escrita no arquivo LDR não permitida.\n");
+        return -EACCES; // Retorna erro ao tentar escrever no arquivo LDR
+    } else if (strcmp(attr_name, "led") == 0) {
+        printk(KERN_INFO "SmartLamp: valor recebido para led é %ld.\n", value);
     }
-
     return strlen(buff);
 }
