7af6926bab40ce5bfe1a59abba167d22bf7fd339
wasp-lahis <lahis.gomes.almeida@gmail.com>
2024-09-11 12:52:30 -0400

    build led update function

--- smartlamp/smartlamp.ino
+++ smartlamp/smartlamp.ino
@@ -1,9 +1,9 @@
 // Definação de variáveis
-int ledPin = 39;
+int ledPin = 5;
 int ledValue = 0;
 
 int ldrPin = 36;
-int ldrValue = 100;
+int ldrValue = 10;
 int ldrMax = 1000;
 
 void setup() {
@@ -34,20 +34,30 @@ void loop() {
 void processCommand(String command) {
     // compare o comando com os comandos possíveis e execute a ação correspondente     
     String driver_command = command;
-    driver_command.trim();                        // remove any \r \n whitespace at the end of the String
-
-    if (driver_command == "GET_LED") 
+    driver_command.trim();  // remove any \r \n whitespace at the end of the String
+    
+    if (driver_command.indexOf("SET_LED") == 0) // encontrou o comando na serial
     {
-      Serial.print("RES GET_LED ");
-      Serial.print(ledValue);
-      Serial.println();
+      int index = driver_command.indexOf("SET_LED");
+      ledValue = driver_command.substring(8).toInt();
+      if (ledValue >= 0 && ledValue <= 100)
+      {
+        ledUpdate(ledValue);
+        Serial.printf("RES SET_LED %d\r\n", ledValue);  
+      }
+      else
+      {
+        Serial.printf("RES SET_LED %d\r\n", -1);  
+      }
+    }
+    else if (driver_command == "GET_LED") 
+    {
+      Serial.printf("RES GET_LED %d\r\n", ledValue);  
     }
     else if (driver_command == "GET_LDR")
     {
       ldrValue = ldrGetValue();
-      Serial.print("RES GET_LDR ");
-      Serial.print(ldrValue);
-      Serial.println();
+      Serial.printf("RES GET_LDR  %d\r\n", ldrValue);  
     }
     else
     {
@@ -57,9 +67,11 @@ void processCommand(String command) {
 }
 
 // Função para atualizar o valor do LED
-void ledUpdate() {
+void ledUpdate(int ledValue) {
     // Valor deve convertar o valor recebido pelo comando SET_LED para 0 e 255
     // Normalize o valor do LED antes de enviar para a porta correspondente
+    int ledValueNormalized = map(ledValue, 0, 100, 0, 255);
+    analogWrite(ledPin, ledValueNormalized);
 }
 
 // Função para ler o valor do LDR
