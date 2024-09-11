81de6f17ee13419f0cbd2726819f2755e8c49e3b
Lahis Almeida <lahis.almeida@indt.local>
2024-09-11 12:12:37 -0400

    receive serial command and get LED and LDR dummy value

--- smartlamp/smartlamp.ino
+++ smartlamp/smartlamp.ino
@@ -1,44 +1,59 @@
-// Defina os pinos de LED e LDR
-// Defina uma variável com valor máximo do LDR (4000)
-// Defina uma variável para guardar o valor atual do LED (10)
+// Definação de variáveis
 int ledPin = 39;
 int ledValue = 0;
 
 int ldrPin = 36;
-// Faça testes no sensor ldr para encontrar o valor maximo e atribua a variável ldrMax
+int ldrValue = 100;
 int ldrMax = 1000;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     
     pinMode(ledPin, OUTPUT);
     pinMode(ldrPin, INPUT);
     
     Serial.printf("SmartLamp Initialized.\n");
-
-
+    delay(2000);
 }
 
 // Função loop será executada infinitamente pelo ESP32
 void loop() {
     //Obtenha os comandos enviados pela serial 
     //e processe-os com a função processCommand
-    Serial.println("Enter data:");
-    while (Serial.available() == 0) {     //wait for data available
-      String teststr = Serial.readString();  //read until timeout
-      teststr.trim();                        // remove any \r \n whitespace at the end of the String
-      if (teststr == "red") {
-        Serial.println("A primary color");
-      } else {
-        Serial.println("Something else");
-      }
+
+    while (Serial.available() == 0)
+    {     // wait for data available
+      String command = Serial.readString();  //read until timeout
+      processCommand(command);
+      //delay(100);
     }
     
 }
 
 
 void processCommand(String command) {
-    // compare o comando com os comandos possíveis e execute a ação correspondente      
+    // compare o comando com os comandos possíveis e execute a ação correspondente     
+    String driver_command = command;
+    driver_command.trim();                        // remove any \r \n whitespace at the end of the String
+
+    if (driver_command == "GET_LED") 
+    {
+      Serial.print("RES GET_LED ");
+      Serial.print(ledValue);
+      Serial.println();
+    }
+    else if (driver_command == "GET_LDR")
+    {
+      ldrValue = ldrGetValue();
+      Serial.print("RES GET_LDR ");
+      Serial.print(ldrValue);
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("ERR Unknown command");
+      
+    }   
 }
 
 // Função para atualizar o valor do LED
@@ -52,4 +67,5 @@ int ldrGetValue() {
     // Leia o sensor LDR e retorne o valor normalizado entre 0 e 100
     // faça testes para encontrar o valor maximo do ldr (exemplo: aponte a lanterna do celular para o sensor)       
     // Atribua o valor para a variável ldrMax e utilize esse valor para a normalização
+    return 100;
 }
